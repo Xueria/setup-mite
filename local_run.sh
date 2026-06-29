3b176d825d384b5e840e19d8f9815c0f6e30ba33
yuchenxue <1816271774@qq.com>
2026-06-29 20:17:46 +0800

    new url

--- local_run.sh
+++ local_run.sh
@@ -1,3 +1,3 @@
-env "download-url"="https://raw.githubusercontent.com/yuchenxue123/builder/main/mite.jar" \
+env "download-url"="https://maven.limingzxc.top/repository/maven-releases/com/mojang/minecraft/1.6.4-MITE/minecraft-1.6.4-MITE.jar" \
     "mite-version"="1.6.4-MITE" \
     node index.js
